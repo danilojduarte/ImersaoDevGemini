c1857d8e1ae9efb70e838efed5a6c87984985a34
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-09-06 15:53:22 -0300

    Criando laço de repetição

--- assets/Js/scrip.js
+++ assets/Js/scrip.js
@@ -1,26 +1,40 @@
 console.log(dados[0].img);
 
-// let section = document.getElementById("resultados-pesquisa");
+let section = document.getElementById("resultados-pesquisa");
 
 
-// for (let dado of dados) {
+for (let dado of dados) {
 
-//   section.innerHTML = `
+  section.innerHTML = `
 
-//         <div class="item-resultado">
-//           <h2 class="">${dados[0].titulo}</h2>
-
-//           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtrrbGQGqhR0xlij_pGeRU8mWVA7MFrckFmQ&s" alt="Capa serie Mr. Robot" class="resultado-capa">
-
-//           <p class="descricao-meta">
-//             ${dado}
-//           </p>
-//           <a href="https://en.wikipedia.org/wiki/Mr._Robot" target="_blank"
-//             >${}</a
-//           >
-//         </div>
-// `;
-
-// }
+        <div class="item-resultado">
+          <h2>
+          ${dados[0].titulo}
+          </h2>
+          <p>
+          ${dados[0].descricao}
+          </p>
 
 
+
+
+
+
+
+
+
+        
+`;
+
+}
+
+
+//   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtrrbGQGqhR0xlij_pGeRU8mWVA7MFrckFmQ&s" alt="Capa serie Mr. Robot" class="resultado-capa">
+
+        //   <p class="descricao-meta">
+        //     ${dado}
+        //   </p>
+        //   <a href="https://en.wikipedia.org/wiki/Mr._Robot" target="_blank"
+        //     >${dados[0].link}</a
+        //   >
+        // </div>
